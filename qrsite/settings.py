de97014fd0b20472ac1017aa8faa0e3ffd77c7e6
prosicon <43424705+prosicon@users.noreply.github.com>
2022-01-21 21:53:19 -0600

    Update settings.py

--- qrsite/settings.py
+++ qrsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vaxy@o*wmv#xq#jpn$yk1fiidmqq7#$t+y&wf(ghul+)(_lv^y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['printex.mx']
 
 
 # Application definition
